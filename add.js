a57244777f680595e778e0239e8666abdaef9cc1
frontendX08 <abdullamamatraimov19@gmail.com>
2023-12-24 20:23:26 +0500

    add the JS 2-dars

---
+++ add.js
@@ -0,0 +1,45 @@
+'use strict'
+// var name = prompt ("Ismingizni kiriting");
+// var nameName = ("Odilbek");
+// var surname = ("odilbek");
+
+
+// if (name == nameName || name == surname) {
+//     console.log(alert ("Sizning ismingiz to'g'ri"));
+// } else {
+//     console.log(alert ("Sizning ismingiz hato"));
+// }
+
+
+
+var one = Number(prompt("1-sonni kiriting"));
+var tue = Number(prompt("2-sonni kiriting"));
+var index = Number(prompt("Amalni kiriting"));
+
+// var a = one + tue;
+// var b = one - tue;
+// var c = one * tue;
+// var d = one / tue;
+
+if (a || b || c || d) {
+    console.log(alert (a = ));
+} else if (b = ) {
+    console.log(alert (b));
+} else if (c) {
+    console.log(alert (c));
+} else if (d) {
+    console.log(alert (d));
+}
+
+
+
+// var name = prompt ("Ismingizni kiriting");
+// var surname =(`Assalomu aleykum:`);
+// var final = (`${surname} ${name}`);
+
+// alert (final);
+
+
+
+
+
